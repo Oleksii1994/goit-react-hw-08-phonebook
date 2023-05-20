3ab78d9a5d02c6ae1ed56f36266d2988b1077e50
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-05-20 03:06:05 +0300

    Update AuthNav.jsx

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 // import css from './AuthNav.module.css';
 import { RegisterLogInLink } from './AuthNav.styled';
 
