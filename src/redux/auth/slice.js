026a659d9b8e559fe47bd5c169bfba1a58e93651
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-05-21 20:22:18 +0300

    optimization

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,5 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import { register, logIn, logOut, refreshUser } from './operations';
+import * as handlersForRegisterLogin from './handlers';
 
 const initialState = {
   user: { name: null, email: null },
@@ -11,34 +12,52 @@ const initialState = {
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  extraReducers: {
-    [register.fulfilled](state, action) {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-      state.isLoggedIn = true;
-    },
-    [logIn.fulfilled](state, action) {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-      state.isLoggedIn = true;
-    },
-    [logOut.fulfilled](state) {
-      state.user = { name: null, email: null };
-      state.token = null;
-      state.isLoggedIn = false;
-    },
-    [refreshUser.pending](state) {
-      state.isRefreshing = true;
-    },
-    [refreshUser.fulfilled](state, action) {
-      state.user = action.payload;
-      state.isLoggedIn = true;
-      state.isRefreshing = false;
-    },
-    [refreshUser.rejected](state) {
-      state.isRefreshing = false;
-    },
+  extraReducers: builder => {
+    builder
+
+      .addCase(logOut.fulfilled, handlersForRegisterLogin.handleLogOutFulfilled)
+
+      .addCase(
+        refreshUser.fulfilled,
+        handlersForRegisterLogin.handleRefreshUserFullfilled
+      )
+      .addMatcher(
+        isAnyOf(refreshUser.pending, refreshUser.rejected),
+        handlersForRegisterLogin.toggleIsRefreshing
+      )
+      .addMatcher(
+        isAnyOf(register.fulfilled, logIn.fulfilled),
+        handlersForRegisterLogin.handleRegisterLoginFulfilled
+      );
   },
+  // extraReducers: {
+  //   [register.fulfilled](state, action) {
+  //     state.user = action.payload.user;
+  //     state.token = action.payload.token;
+  //     state.isLoggedIn = true;
+  //   },
+  //   [logIn.fulfilled](state, action) {
+  //     state.user = action.payload.user;
+  //     state.token = action.payload.token;
+  //     state.isLoggedIn = true;
+  //   },
+  //   [logOut.fulfilled](state) {
+  //     state.user = { name: null, email: null };
+  //     state.token = null;
+  //     state.isLoggedIn = false;
+  //   },
+  //   [refreshUser.pending](state) {
+  //     state.isRefreshing = true;
+  //   },
+  //   [refreshUser.fulfilled](state, action) {
+  //     state.user = action.payload;
+  //     state.isLoggedIn = true;
+  //     state.isRefreshing = false;
+  //   },
+  //   [refreshUser.rejected](state) {
+  //     state.isRefreshing = false;
+  //   },
+  // },
 });
 
 export const authReducer = authSlice.reducer;
